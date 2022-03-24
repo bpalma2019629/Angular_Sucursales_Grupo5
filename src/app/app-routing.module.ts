b891eba9c6b7f1d467ab41595fd2c4a005cac549
bpalma <bpalma-2019629@kinal.edu.gt>
2022-03-24 14:18:28 -0600

    Edicion Final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,6 @@ import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 
-//Rutas y Redireccionamiento Enrique Hurtarte 2020020
-
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
